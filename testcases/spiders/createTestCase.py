a1117137a9bc903ebeda525160d7f8ad4e6bb852
Esteban <esteban_fs@hotmail.com>
2018-01-12 15:33:43 -0500

    Test

--- testcases/spiders/createTestCase.py
+++ testcases/spiders/createTestCase.py
@@ -9,21 +9,21 @@ class createTestCaseSpider(scrapy.Spider):
     http_user = settings.get('HTTP_USER')
     http_pass = settings.get('HTTP_PASS')
     allowed_domains = ["confluence.verndale.com"]
-    start_urls = ['https://confluence.verndale.com/display/GEHC/Course+Overview+Module']
+    start_urls = ['https://confluence.verndale.com/display/GEHC/Course+Offering+Module']
 
     def parse(self, response):
-        # components = response.xpath('//*[@id="main-content"]/div/div[4]/div/div/div[1]/table/tbody/tr')
-        # for row in components:
-        #     item = row.select('.//text()').extract()
-        #     print('Verify '+item[1]+' component')
+        components = response.xpath('//*[@id="main-content"]/div/div[4]/div/div/div[1]/table/tbody/tr')
+        for row in components:
+            item = row.select('.//text()').extract()
+            print('Verify '+item[1]+' component')
 
-        for url in self.start_urls:
-            # Xpath String for Requirements
-            xpathstring = "//*[@id='main-content']/div/div[4]/div/div/div[1]/table/tbody/tr[4]/td[3]/div[contains(@class,'content-wrapper')]"
-            requirements = response.xpath(xpathstring + "/ul/li/text() | " + xpathstring + "/ul/li/span/text()"
-                                          ).extract()
-
-            print(requirements)
+        # for url in self.start_urls:
+        #     # Xpath String for Requirements
+        #     xpathstring = "//*[@id='main-content']/div/div[4]/div/div/div[1]/table/tbody/tr[4]/td[3]/div[contains(@class,'content-wrapper')]"
+        #     requirements = response.xpath(xpathstring + "/ul/li/text() | " + xpathstring + "/ul/li/span/text()"
+        #                                   ).extract()
+        #
+        #     print(requirements)
 
 
             # for row in requirements:
