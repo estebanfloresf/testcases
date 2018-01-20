6fbd206e9280941e4e53b0cc98ed266c7bfa7c07
Esteban <esteban_fs@hotmail.com>
2018-01-19 19:35:10 -0500

    Test

--- testcases/spiders/testSpider.py
+++ testcases/spiders/testSpider.py
@@ -16,13 +16,10 @@ class TestspiderSpider(scrapy.Spider):
         item = TestCasesItem()
         table = response.xpath('//*[@id="main-content"]/div/div[4]/div/div/div[1]/table/tbody/tr')
 
-
-        for index,row in enumerate(table):
+        for index, row in enumerate(table):
             component = row.select('.//td[2]/text() | .//td[2]/p/text()').extract_first()
-            print('\n'+str(index)+' '+str(component))
+            print('\n' + str(index) + ' ' + str(component))
             item['component'] = component
-
-
 
             responsive_req = row.xpath(".//td[3]/div[contains(@class,'content-wrapper')]")
             # wordstolook = ['Recommendation:', 'Optional', 'Required', 'Standard Value:', 'Note:', 'Notes:',
@@ -33,7 +30,7 @@ class TestspiderSpider(scrapy.Spider):
             responsive = {}
             resp_devices = []
             resp_general = []
-            resp_dev_general = []
+            resp_devices_req = []
 
             path = ".//div[contains(@class,'confluence-information-macro confluence-information-macro-information conf-macro output-block')]"
             for req in responsive_req.xpath(path):
@@ -50,20 +47,22 @@ class TestspiderSpider(scrapy.Spider):
                     #         print( p.select('.//text()').extract())
                     # print(devices)
 
-                    if(elem.xpath("./ul")):
-                        self.seekNestedLists(elem)
-                       
+                    if (elem.xpath("./ul")):
+                        resp_devices_req = self.seekNestedLists(elem,index)
+            print(list(zip(resp_devices,resp_devices_req)))
 
 
-    def seekNestedLists(self,path):
-        
-        for ul in  path.xpath("./ul"):
+    def seekNestedLists(self, path,index):
+        all = []
+        for indexUL,ul in enumerate(path.xpath("./ul")):
 
-            if (ul.xpath('./li/text() | ./li/span/text()')):
-                # for li in  ul.xpath('./li/text() | ./li/span/text()').extract():
-                #     # print(str(index)+str(liNum+1)+' '+li)
-                #     print("req %s.%s %s" %(index,liNum+1,li))
-                print(ul.xpath('./li/text() | ./li/span/text()').extract())
 
-            if(ul.xpath('./li/ul')):
-                self.seekNestedLists(ul.xpath('./li'))
+            if (ul.xpath("./li/text() | ./li/span[not(contains(@class,'confluence-embedded-file-wrapper confluence-embedded-manual-size'))]/text()")):
+                # for indexLI,li in enumerate(ul.xpath('./li/text() | ./li/span/text()').extract()):
+                all.append(ul.xpath('./li/text() | ./li/span/text()').extract())
+        return all
+                # if (ul.xpath('./li/ul')):
+                #         self.seekNestedLists(ul.xpath('./li'),str(index)+'-'+str(indexLI+1))
+
+
+
